71d2498ab80702b57ac7b257231eef20227f5c9c
Tusha Samaniya <tusharsamaniya02@gmail.com>
2026-06-25 17:01:06 +0530

    add the tailwind config class

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: 'class', // Step 8
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Step 5
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
