bbef0444649235b48707f67fb00fd92c96708c4e
Tusha Samaniya <tusharsamaniya02@gmail.com>
2026-06-25 17:32:37 +0530

    created the main class

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
+import './index.css'
+
+// Create a client for React Query
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
